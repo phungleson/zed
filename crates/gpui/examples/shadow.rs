0c184b15cac61e7b4d2d9ad859baaf515c3fa3e4
Son <leson.phung@gmail.com>
2024-07-30 15:10:26 +1000

    Add shadow example

--- crates/gpui/examples/shadow.rs
+++ crates/gpui/examples/shadow.rs
@@ -1,4 +1,5 @@
 use gpui::*;
+use smallvec::smallvec;
 
 struct Shadow {}
 
@@ -10,7 +11,31 @@ impl Render for Shadow {
             .size_full()
             .justify_center()
             .items_center()
+            .size(Length::Definite(Pixels(300.0).into()))
+            .justify_center()
+            .items_center()
             .child(div().size_8().shadow_sm())
+            .child(
+                div()
+                    .size_4()
+                    .shadow_lg()
+                    .rounded_full()
+                    .bg(rgb(0x00ffff))
+                    .shadow(smallvec![
+                        BoxShadow {
+                            color: hsla(0., 0., 0., 1.0),
+                            offset: point(px(16.), px(16.)),
+                            blur_radius: px(1.0),
+                            spread_radius: px(0.),
+                        },
+                        BoxShadow {
+                            color: hsla(0., 0., 0., 1.0),
+                            offset: point(px(32.), px(16.)),
+                            blur_radius: px(0.0),
+                            spread_radius: px(0.),
+                        }
+                    ]),
+            )
     }
 }
 
