34dced3c953abb3f2d1c1c1f350d1fdeb8ae8865
Son <leson.phung@gmail.com>
2024-08-06 07:12:22 +1000

    Screenshot WIP

--- crates/gpui/examples/screenshot.rs
+++ crates/gpui/examples/screenshot.rs
@@ -1,9 +1,9 @@
+use gpui::*;
+use image::codecs::png;
+use image::{ExtendedColorType, ImageEncoder};
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::PathBuf;
-use image::codecs::png;
-use image::{ExtendedColorType, ImageEncoder};
-use gpui::*;
 
 struct Main {
     text: SharedString,
@@ -29,7 +29,10 @@ impl Render for Main {
 fn main() {
     App::new().run(|cx: &mut AppContext| {
         let bounds = Bounds::centered(None, size(px(300.0), px(300.0)), cx);
-        cx.open_window(
+        cx.on_action(screenshot);
+        cx.bind_keys(vec![KeyBinding::new("s", Screenshot, None)]);
+
+        let window = cx.open_window(
             WindowOptions {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
                 ..Default::default()
@@ -42,11 +45,24 @@ fn main() {
         )
         .unwrap();
 
-        cx.on_action(screenshot);
-        cx.set_menus(vec![Menu {
-            name: "screenshot".into(),
-            items: vec![MenuItem::action("Screenshot", Screenshot)],
-        }]);
+        // cx.set_menus(vec![Menu {
+        //     name: "screenshot".into(),
+        //     items: vec![MenuItem::action("Screenshot", Screenshot)],
+        // }]);
+        // cx.observe_keystrokes(move |ev, cx| {
+        //     window
+        //         .update(cx, |view, cx| {
+        //             view.recent_keystrokes.push(ev.keystroke.clone());
+        //             cx.notify();
+        //         })
+        //         .unwrap();
+        // })
+        // .detach();
+        window
+            .update(cx, |view, cx| {
+                cx.activate(true);
+            })
+            .unwrap();
     });
 }
 
@@ -54,18 +70,35 @@ actions!(screenshot, [Screenshot]);
 
 fn screenshot(_: &Screenshot, cx: &mut AppContext) {
     println!("screenshot");
-    println!("window_id: {:?}", cx.windows().first().cloned().unwrap().window_id());
-    let window_ids = cx.windows().iter().map(|window| window.window_id()).collect::<Vec<_>>();
+    println!(
+        "window_id: {:?}",
+        cx.windows().first().cloned().unwrap().window_id()
+    );
+    let window_ids = cx
+        .windows()
+        .iter()
+        .map(|window| window.window_id())
+        .collect::<Vec<_>>();
     println!("window_ids: {:?}", window_ids);
 
-    cx.active_window().unwrap().update(cx, |_, cx| {
-        let screenshot = cx.screenshot();
-        println!("screenshot");
-        let out_file = PathBuf::from("out.png");
-        let file = File::create(&out_file).unwrap();
-        let ref mut writer = BufWriter::new(file);
+    cx.active_window()
+        .unwrap()
+        .update(cx, |_, cx| {
+            let screenshot = cx.screenshot();
+            println!("screenshot");
+            let out_file = PathBuf::from("out.png");
+            let file = File::create(&out_file).unwrap();
+            let ref mut writer = BufWriter::new(file);
 
-        let encoder = png::PngEncoder::new(writer);
-        encoder.write_image(&screenshot.data, screenshot.width, screenshot.height, ExtendedColorType::Rgb8).unwrap();
-    }).unwrap();
+            let encoder = png::PngEncoder::new(writer);
+            encoder
+                .write_image(
+                    &screenshot.data,
+                    screenshot.width,
+                    screenshot.height,
+                    ExtendedColorType::Rgb8,
+                )
+                .unwrap();
+        })
+        .unwrap();
 }
