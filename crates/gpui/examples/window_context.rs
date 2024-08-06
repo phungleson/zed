63212b559a0f60420ed477a7e80516bfe02df08f
Son <leson.phung@gmail.com>
2024-08-06 20:28:07 +1000

    Try to use keybindings

--- crates/gpui/examples/window_context.rs
+++ crates/gpui/examples/window_context.rs
@@ -1,51 +1,60 @@
 use gpui::*;
 
-struct Main {}
+struct Main {
+    focus_handle: FocusHandle
+}
+
+impl Main {
+    // fn backspace(&mut self, _: &Backspace, cx: &mut ViewContext<Self>) {
+    //     if self.selected_range.is_empty() {
+    //         self.select_to(self.previous_boundary(self.cursor_offset()), cx)
+    //     }
+    //     self.replace_text_in_range(None, "", cx)
+    // }
+
+    fn backspace(&mut self, _: &Backspace, cx: &mut ViewContext<Self>) {
+        println!("{}", cx.window_context().is_window_active());
+    }
+}
+
+impl FocusableView for Main {
+    fn focus_handle(&self, _: &AppContext) -> FocusHandle {
+        self.focus_handle.clone()
+    }
+}
 
 impl Render for Main {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        div().flex().bg(rgb(0x2e7d32))
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .track_focus(&self.focus_handle)
+            .bg(rgb(0x2e7d32))
+            .key_context("TextInput")
+            .on_action(cx.listener(Self::backspace))
     }
 }
 
 fn main() {
     App::new().run(|cx: &mut AppContext| {
-        let bounds = Bounds::centered(None, size(px(300.0), px(300.0)), cx);
-        cx.on_action(check_window_context);
-        cx.set_menus(vec![Menu {
-            name: "Action".into(),
-            items: vec![MenuItem::action("CheckWindowContext", CheckWindowContext)],
-        }]);
+        cx.bind_keys([
+            KeyBinding::new("backspace", Backspace, None),
+        ]);
 
+        let bounds = Bounds::centered(None, size(px(300.0), px(300.0)), cx);
         let window = cx
             .open_window(
                 WindowOptions {
                     window_bounds: Some(WindowBounds::Windowed(bounds)),
                     ..Default::default()
                 },
-                |cx| cx.new_view(|_cx| Main {}),
+                |cx| {
+                    cx.new_view(|cx| Main {
+                        focus_handle: cx.focus_handle()
+                    })
+                },
             )
-            .unwrap();
-
-        window
-            .update(cx, |_, cx| {
-                cx.activate(true);
-            })
             .unwrap();
     });
 }
 
-actions!(action, [CheckWindowContext]);
-
-fn check_window_context(_: &CheckWindowContext, cx: &mut AppContext) {
-    // I would like to access WindowContext, the current API seem to expose it only via #update
-    cx.active_window()
-        .unwrap()
-        .update(cx, |_, _| println!("Nothing wrong"))
-        .expect("window somehow not found?");
-    // This doesn't work either
-    // cx.update_window(cx.active_window().unwrap(), |_, _cx| {
-    //     println!("Nothing wrong")
-    // })
-    // .expect("window somehow not found?");
-}
+actions!(action, [Backspace]);
